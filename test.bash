098f30397614842ae3276fbc21176ed0cf46106c
Nobuhiro MIKI <nob@bobuhiro11.net>
2023-04-05 05:30:11 +0000

    test.bash: Add more outputs.
    Signed-off-by: Nobuhiro MIKI <nob@bobuhiro11.net>

--- test.bash
+++ test.bash
@@ -1,9 +1,14 @@
 #!/bin/bash -xe
 
 openstack compute service list
+openstack network agent list
 net_id=$(openstack network show private -f json 2>/dev/null | jq -r .id | tr -d "\r\n")
 nova boot --image cirros-0.5.2-x86_64-disk --flavor m1.medium --nic net-id=$net_id testvm
 sleep 10
 nova list
 nova list | grep ACTIVE
+nova show testvm
 nova delete testvm
+
+sudo journalctl --no-pager -n 300 -eu devstack@n-cpu
+sudo journalctl --no-pager -n 300 -eu devstack@q-agt
