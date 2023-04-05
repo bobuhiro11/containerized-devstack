941871302775db03289a456b4d6ff747ba496b8d
Nobuhiro MIKI <nob@bobuhiro11.net>
2023-04-05 06:21:41 +0000

    fix for debug
    Signed-off-by: Nobuhiro MIKI <nob@bobuhiro11.net>

--- test.bash
+++ test.bash
@@ -6,7 +6,7 @@ net_id=$(openstack network show private -f json 2>/dev/null | jq -r .id | tr -d 
 nova boot --image cirros-0.5.2-x86_64-disk --flavor m1.medium --nic net-id=$net_id testvm
 sleep 10
 nova list
-nova list | grep ACTIVE
+# nova list | grep ACTIVE
 nova show testvm
 nova delete testvm
 
