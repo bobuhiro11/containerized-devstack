a4a415433b0af2188e96445c38128fe98458c7ff
Nobuhiro MIKI <nob@bobuhiro11.net>
2023-04-14 15:44:36 +0900

    Add restarting q-l3/q-meta and related tests
    devstack@q-l3 and devstack@q-meta would not reconnect
    to RabbitMQ, restart them manually. Also check if the
    VM can get an IP address via DHCP and if it can connect
    to Metadata API.
    
    Signed-off-by: Nobuhiro MIKI <nob@bobuhiro11.net>

--- test.bash
+++ test.bash
@@ -1,14 +1,24 @@
 #!/bin/bash -xe
 
 nova-manage cell_v2 discover_hosts --verbose
+
+sudo systemctl restart devstack@q-l3
+sudo systemctl restart devstack@q-meta
+
+sleep 10
 openstack compute service list
 openstack network agent list
+
 net_id=$(openstack network show private -f json 2>/dev/null | jq -r .id | tr -d "\r\n")
-nova boot --image cirros-0.5.2-x86_64-disk --flavor m1.medium --nic net-id=$net_id testvm
-sleep 15
+nova boot --image cirros-0.5.2-x86_64-disk --flavor m1.medium \
+  --nic net-id=$net_id --availability-zone=nova:controller testvm
+sleep 60
+
 nova list
-nova show testvm
-nova list | grep ACTIVE
+nova show testvm | grep ACTIVE
+nova console-log testvm
+nova console-log testvm | grep "udhcpc: lease of"
+nova console-log testvm | grep "successful after"
 nova delete testvm
 
 sudo journalctl --no-pager -n 300 -eu devstack@n-cpu
